3536f9137eeee31ec05ca2b9109b02a776c4dda8
Tomas Perez <tom@0x101.com>
2012-07-22 02:02:20 +0400

    Moving functionality to the node-api-client #7. @see https://github.com/tomasperezv/node-api-client

--- server/geo-twitter.js
+++ server/geo-twitter.js
@@ -11,70 +11,26 @@
  * Public Domain.
  * No warranty expressed or implied. Use at your own risk.
  */
-var http = require('http'),
-	querystring = require('querystring');
+var ApiClientFactory = require('./node-api-client/api-client').ApiClientFactory,
+	ApiClientTypes = require('./node-api-client/api-client').Types;
 
 var GeoTwitter = function(config) {
-	/**
-	 * Used to compose the filters query string
-	 */
-	this.AND = ' AND ';
-	this.OR = ' OR ';
 	this.config = config;
 };
 
 GeoTwitter.prototype = {
-	/**
-	 * @return {String} host
-	 */
-	_getTwitterSearchHost: function() {
-		return this.config['twitter-search-url'];
-	},
-	
-	/**
-	 * @param {Object} filters
-	 * @return {String} filters urlencoded
-	 */
-	_formatFilters: function(filters) {
-		return querystring.stringify({'q': filters['data'].join(filters['type'])});
-	},
 
 	/**
-	 * Return tweets based on the filters, asking to the Twitter Search API.
-	 * @see https://dev.twitter.com/docs/api/1/get/search
-	 * @param {Function} callback
-	 * @param {Object} filters
+	 * Instantiate a twitter search client.
+	 * @author tom@0x101.com
+	 * @return {ApiClient}
 	 */
-	_getTweets: function(callback, filters, page) {
-
-		var options = {
-			host: this._getTwitterSearchHost(),
+	_getTwitterSearchClient: function() {
+		var config = {
+			host: this.config['twitter-search-url'],
 			port: 80,
-			path: '/search.json?' + this._formatFilters(filters) + '&rpp=' + this.config['twitter-max-tweets'] + '&page=' + page,
-			method: 'POST'
 		};
-
-		console.log('Asking to the Twitter Search API: ' + options.path);
-
-		var req = http.request(options, function(res) {
-			var output = '';
-			res.setEncoding('utf8');
-
-			res.on('data', function (chunk) {
-				output += chunk;
-			});
-
-			res.on('end', function() {
-				console.log('Successful request to Twitter Search API:' + res.statusCode + ' ' + output.length + ' bytes');
-				callback(output);
-			});
-		});
-
-		req.on('error', function(e) {
-			console.log(e.message);
-		});
-		
-		req.end();
+		return ApiClientFactory.get(ApiClientTypes.TWITTER_SEARCH, config);
 	},
 
 	/**
@@ -84,18 +40,9 @@ GeoTwitter.prototype = {
 	 * @param {Object} params
 	 */
 	filterTweets: function(callback, params) {
-		var type = this.AND;
-		if (params.type_and == 'false') {
-			type = this.OR;
-		}
-		var filters = {
-			'data': params.filters.split(' '),
-			'type': type
-		};
-		if (typeof params.page === 'undefined') {
-			params.page = 1;
-		}
-		this._getTweets(callback, filters, params.page);
+		params.maxTweets = this.config['twitter-max-tweets'];
+		var twitterSearchClient = this._getTwitterSearchClient();
+		twitterSearchClient.search(params, callback);
 	}
 };
 
@@ -104,14 +51,15 @@ GeoTwitter.prototype = {
  */
 module.exports = function(config) {
 	/**
-	 * Initialize the configuration
+	 * Initialize the configuration, if needed.
 	 * @var constants
 	 */
 	if(typeof(config) === 'undefined') {
 		var Setup = Setup || require('../../0x101.com-www/setup.js'),
 			basedir = Setup.serverDirectory,
 			Config = Config || require('../' + basedir + '/config.js');
-		config = Config.get('geo-twitter');
+
+		var config = Config.get('geo-twitter');
 	}
 	return new GeoTwitter(config);
 };
--- server/test/client-test.js
+++ server/test/client-test.js
@@ -12,9 +12,9 @@
 // Mock the Setup and Config, normally exposed by Blackbriar.
 var mockConfig = {
 	"twitter-search-url": "search.twitter.com",
-	"twitter-max-tweets": 2
+	"twitter-max-tweets": 1
 };
-var geoTwitter = require('./geo-twitter.js')(mockConfig);
+var geoTwitter = require('../geo-twitter.js')(mockConfig);
 
 // Test a simple request
 var params = {
