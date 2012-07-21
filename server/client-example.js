d852c9230e7d472cc6115c85d005a77c6a61bd3f
Tomas Perez <tom@0x101.com>
2012-07-21 23:39:39 +0400

    Command line client for testing the geo-twitter API, #7.

---
+++ server/client-example.js
@@ -0,0 +1,27 @@
+/**
+ * Node.js client application for test the connectivity with
+ * the external API's.
+ *
+ * @author tom@0x101.com
+ * @see https://github.com/tomasperezv/global-trends
+ *
+ * Public Domain.
+ * No warranty expressed or implied. Use at your own risk.
+ */
+
+// Mock the Setup and Config, normally exposed by Blackbriar.
+var mockConfig = {
+	"twitter-search-url": "search.twitter.com",
+	"twitter-max-tweets": 2
+};
+var geoTwitter = require('./geo-twitter.js')(mockConfig);
+
+// Test a simple request
+var params = {
+	type_and: true,
+	filters: 'node server',
+};
+
+geoTwitter.filterTweets(function(result){
+	console.log(result);
+}, params);
--- server/geo-twitter.js
+++ server/geo-twitter.js
@@ -12,90 +12,106 @@
  * No warranty expressed or implied. Use at your own risk.
  */
 var http = require('http'),
-	querystring = require('querystring'),
-	Setup = require('../../0x101.com-www/setup.js'),
-	basedir = Setup.serverDirectory,
-	Config = require('../' + basedir + "/config.js");
+	querystring = require('querystring');
 
-/**
- * Initialize the configuration
- * @var constants
- */
-this.config = Config.get('geo-twitter');
-
-/**
- * Used to compose the filters query string
- */
-this.AND = ' AND ';
-this.OR = ' OR ';
-
-/**
- * @return {String} host
- */
-this._getTwitterSearchHost= function() {
-	return this.config['twitter-search-url'];
+var GeoTwitter = function(config) {
+	/**
+	 * Used to compose the filters query string
+	 */
+	this.AND = ' AND ';
+	this.OR = ' OR ';
+	this.config = config;
 };
 
-/**
- * @param {Object} filters
- * @return {String} filters urlencoded
- */
-this._formatFilters = function(filters) {
-	return querystring.stringify({'q': filters['data'].join(filters['type'])});
-};
-
-/**
- * Return tweets based on the filters, asking to the Twitter Search API.
- * @see https://dev.twitter.com/docs/api/1/get/search
- * @param {Function} callback
- * @param {Object} filters
- */
-this._getTweets = function(callback, filters, page) {
-
-	var options = {
-		host: this._getTwitterSearchHost(),
-		port: 80,
-		path: '/search.json?' + this._formatFilters(filters) + '&rpp=' + this.config['twitter-max-tweets'] + '&page=' + page,
-		method: 'POST'
-	};
-
-	console.log('Asking to the Twitter Search API: ' + options.path);
-
-	var req = http.request(options, function(res) {
-		var output = '';
-		res.setEncoding('utf8');
-
-		res.on('data', function (chunk) {
-			output += chunk;
-		});
-
-		res.on('end', function() {
-			console.log('Successful request to Twitter Search API:' + res.statusCode + ' ' + output.length + ' bytes');
-			callback(output);
-		});
-	});
-
-	req.on('error', function(e) {
-		console.log(e.message);
-	});
+GeoTwitter.prototype = {
+	/**
+	 * @return {String} host
+	 */
+	_getTwitterSearchHost: function() {
+		return this.config['twitter-search-url'];
+	},
 	
-	req.end();
+	/**
+	 * @param {Object} filters
+	 * @return {String} filters urlencoded
+	 */
+	_formatFilters: function(filters) {
+		return querystring.stringify({'q': filters['data'].join(filters['type'])});
+	},
+
+	/**
+	 * Return tweets based on the filters, asking to the Twitter Search API.
+	 * @see https://dev.twitter.com/docs/api/1/get/search
+	 * @param {Function} callback
+	 * @param {Object} filters
+	 */
+	_getTweets: function(callback, filters, page) {
+
+		var options = {
+			host: this._getTwitterSearchHost(),
+			port: 80,
+			path: '/search.json?' + this._formatFilters(filters) + '&rpp=' + this.config['twitter-max-tweets'] + '&page=' + page,
+			method: 'POST'
+		};
+
+		console.log('Asking to the Twitter Search API: ' + options.path);
+
+		var req = http.request(options, function(res) {
+			var output = '';
+			res.setEncoding('utf8');
+
+			res.on('data', function (chunk) {
+				output += chunk;
+			});
+
+			res.on('end', function() {
+				console.log('Successful request to Twitter Search API:' + res.statusCode + ' ' + output.length + ' bytes');
+				callback(output);
+			});
+		});
+
+		req.on('error', function(e) {
+			console.log(e.message);
+		});
+		
+		req.end();
+	},
+
+	/**
+	 * Filter tweets based on user defined parameters.
+	 * @author tom@0x101.com
+	 * @param {Function} callback
+	 * @param {Object} params
+	 */
+	filterTweets: function(callback, params) {
+		var type = this.AND;
+		if (params.type_and == 'false') {
+			type = this.OR;
+		}
+		var filters = {
+			'data': params.filters.split(' '),
+			'type': type
+		};
+		if (typeof params.page === 'undefined') {
+			params.page = 1;
+		}
+		this._getTweets(callback, filters, params.page);
+	}
 };
 
 /**
- * Filter tweets based on user defined parameters.
- * @author tom@0x101.com
- * @param {Function} callback
- * @param {Object} params
+ * @param {Object} options
  */
-this.filterTweets = function(callback, params) {
-	var type = this.AND;
-	if (params.type_and == 'false') {
-		type = this.OR;
+module.exports = function(config) {
+	/**
+	 * Initialize the configuration
+	 * @var constants
+	 */
+	if(typeof(config) === 'undefined') {
+		var Setup = Setup || require('../../0x101.com-www/setup.js'),
+			basedir = Setup.serverDirectory,
+			Config = Config || require('../' + basedir + '/config.js');
+		config = Config.get('geo-twitter');
 	}
-	var filters = {
-		'data': params.filters.split(' '),
-		'type': type
-	};
-	this._getTweets(callback, filters, params.page);
+	return new GeoTwitter(config);
 };
